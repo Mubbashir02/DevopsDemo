1fe3504404f065f482cdf8aa042b314d16be124e
Mubbashir02 <mubismarty@gmail.com>
2022-12-31 08:07:14 +0500

    commit all items

---
+++ main-test.py
@@ -0,0 +1,9 @@
+from main import Add
+
+def check():
+    assert Add(2,4) == 6
+    print('ok')
+
+
+if __name__ == '__main__':
+    check()
---
+++ main.py
@@ -0,0 +1,11 @@
+def Add(a,b):
+    return a+b
+
+
+
+def Show():
+    print('ok ')
+
+
+if __name__ == '__main__':
+    Show()
